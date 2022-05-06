8426e76fb178df2249c519b28680b735f0c8156a
Milashevskyi Bohdan <milasevskijbogdan@gmail.com>
2022-05-06 20:20:16 +0300

    implementation of calculator functions

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,5 +7,23 @@ int main()
 	int num1 = 10, num2 = 20;
 	int result = addition(num1, num2);
 	cout << "Result: " << result << endl;
+
+	result = substraction(num1, num2);
+	cout << "Result: " << result << endl;
+
+	result = multiplication(num1, num2);
+	cout << "Result: " << result << endl;
+
+
+	try
+	{
+		result = division(num1, num2);
+		cout << "Result: " << result << endl;
+	}
+	catch (...)
+	{
+		cout << "Error occured!" << endl;
+	}
+
 }
 
--- ConsoleApplication1/Header.h
+++ ConsoleApplication1/Header.h
@@ -4,3 +4,6 @@
 using namespace std;
 
 int addition(int, int);
+int substraction(int, int);
+int multiplication(int, int);
+int division(int, int);
